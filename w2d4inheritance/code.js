b2f92bff5c20d7b1a442e5dbf372f530dc580353
yaomengting <33788209+yaomengting@users.noreply.github.com>
2021-10-08 14:07:01 -0500

    functional programing, form css

--- w2d4inheritance/code.js
+++ w2d4inheritance/code.js
@@ -1,10 +1,34 @@
 String.prototype.filter = function (banned) {
   const arr = this.toString().split(" ");
-  return arr
-    .filter((a) => !banned.includes(a)).join(" ");
-    
+  return arr.filter((a) => !banned.includes(a)).join(" ");
 };
 
 //console.log("This house is not nice".filter(["not", "nice"]));
 
-//Array.prototype.bubbleSort
+Array.prototype.bubbleSort = function () {
+  for (var i = 0; i < this.length; i++) {
+    for (var j = 0; j < this.length - i - 1; j++) {
+      if (this[j] > this[j + 1]) {
+        var temp = this[j];
+        this[j] = this[j + 1];
+        this[j + 1] = temp;
+      }
+    }
+  }
+  return this;
+};
+
+var Person = function () {};
+
+Person.prototype.initialize = function (name, age) {
+  this.name = name;
+  this.age = age;
+};
+
+// TODO: create the class Teacher and a method teach
+var Teacher = function () {};
+Teacher.prototype = new Person();
+Teacher.prototype.teach = function (subject) {
+  return this.name + " is now teaching " + subject;
+};
+// export Teacher;
--- w2d4inheritance/tests.js
+++ w2d4inheritance/tests.js
@@ -1,10 +1,22 @@
-
-
 describe("filter", function () {
-  it("return a array", function () {
+  it("return filtered array without a list of banned words", function () {
     assert.equal(
-      'This house is',
+      "This house is",
       "This house is not nice".filter(["not", "nice"])
     );
+  });
+});
+
+describe("bubble sort", function () {
+  it("return a sorted array using algorithms", function () {
+    assert.deepEqual([1, 2, 3, 4, 5, 6], [1, 2, 3, 6, 5, 4].bubbleSort());
+  });
+});
+
+describe("inheritance", function () {
+  it("create new object and gave it function", function () {
+    var him = new Teacher();
+    him.initialize("Adam", 45);
+    assert.equal("Adam is now teaching Inheritance", him.teach("Inheritance"));
   });
 });
